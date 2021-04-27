c44335f741755ca780acb0bd33fef3c7e254c665
salgr <salgr@LAPTOP-CHK7GE6L>
2021-04-27 18:33:30 +0200

    try

--- TreballIPC/src/treballipc/FXMLDocumentController.java
+++ TreballIPC/src/treballipc/FXMLDocumentController.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import javafx.beans.value.ObservableValue;
 import java.util.ResourceBundle;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,6 +19,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
 
 public class FXMLDocumentController {
 
@@ -46,7 +50,57 @@ public class FXMLDocumentController {
     private Button multiplayerButton;
     @FXML
     private Button logoutButton;
+    
+    @FXML
+    private HBox panel;
+    @FXML
+    private VBox vbox1;
 
+    @FXML
+    private GridPane grid1;
+
+    @FXML
+    private VBox vbox2;
+
+    @FXML
+    private GridPane grid2;
+
+    @FXML
+    private VBox vbox3;
+
+    @FXML
+    private GridPane grid3;
+
+    @FXML
+    private VBox vbox4;
+
+    @FXML
+    private GridPane grid4;
+
+    @FXML
+    private VBox vbox5;
+
+    @FXML
+    private GridPane grid5;
+
+    @FXML
+    private VBox vbox6;
+
+    @FXML
+    private GridPane grid6;
+
+    @FXML
+    private VBox vbox7;
+
+    @FXML
+    private GridPane grid7;
+
+    @FXML
+    private VBox vbox8;
+
+    @FXML
+    private GridPane grid8;
+    
     @FXML
     void loginStart(MouseEvent event) throws Exception{
         
@@ -94,16 +148,35 @@ public class FXMLDocumentController {
 
     public static SimpleBooleanProperty loggedIn = new SimpleBooleanProperty(false);
 
+    
+    
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        
         assert registerButton != null : "fx:id=\"registerButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
         assert loginButton != null : "fx:id=\"loginButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
-        assert staticsButton != null : "fx:id=\"staticsButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
-        assert highContrast != null : "fx:id=\"highContrast\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
         assert singleplayerButton != null : "fx:id=\"singleplayerButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
         assert multiplayerButton != null : "fx:id=\"multiplayerButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
-        assert logoutButton != null : "fx:id=\"logout\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
-        
+        assert staticsButton != null : "fx:id=\"staticsButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert highContrast != null : "fx:id=\"highContrast\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert panel != null : "fx:id=\"panel\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox1 != null : "fx:id=\"vbox1\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid1 != null : "fx:id=\"grid1\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox2 != null : "fx:id=\"vbox2\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid2 != null : "fx:id=\"grid2\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox3 != null : "fx:id=\"vbox3\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid3 != null : "fx:id=\"grid3\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox4 != null : "fx:id=\"vbox4\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid4 != null : "fx:id=\"grid4\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox5 != null : "fx:id=\"vbox5\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid5 != null : "fx:id=\"grid5\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox6 != null : "fx:id=\"vbox6\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid6 != null : "fx:id=\"grid6\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox7 != null : "fx:id=\"vbox7\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid7 != null : "fx:id=\"grid7\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert vbox8 != null : "fx:id=\"vbox8\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
+        assert grid8 != null : "fx:id=\"grid8\" was not injected: check your FXML file 'FXMLDocument.fxml'.";
         
         
         loggedIn.addListener( //EXEMPLE DE LISTENER!!!!
@@ -123,6 +196,141 @@ public class FXMLDocumentController {
                     logoutButton.setVisible(false);
                 }
         });
+        
             
+        TreballIPC.playing.addListener(new ChangeListener(){
+                public void changed(ObservableValue o,Object oldVal, 
+                 Object newVal){
+                    if(TreballIPC.playing.getValue()) {
+                        panel.setDisable(false);
+                }
+                    else {
+                        panel.setDisable(true);
+                    }
+                }
+                });
+                }
+    ///// funcions del panel de joc no es bonic pero es el que hi ha
+    
+    
+    
+    
+    @FXML
+    void vbox1click(MouseEvent event) {
+    }
+
+    @FXML
+    void vbox1hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox1nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox2click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox2hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox2nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox3click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox3hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox3nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox4click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox4hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox4nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox5click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox5hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox5nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox6click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox6hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox6nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox7click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox7hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox7nohover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox8click(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox8hover(MouseEvent event) {
+
+    }
+
+    @FXML
+    void vbox8nohover(MouseEvent event) {
+
     }
 }
